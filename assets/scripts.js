421f0b59f46b93daec94e4c036b4fbeb0a36e10a
Matúš <kollarm9@gmail.com>
2023-06-02 15:35:34 +0200

    added functioning mobile navbar with js added css for navbar and navbar controllers

---
+++ assets/scripts.js
@@ -0,0 +1,35 @@
+const mobileNav = document.querySelector(".mobile-nav");
+const mobileHam = document.querySelector(".mobile-ham");
+const html = document.querySelector("html");
+const mobileCancel = document.querySelector(".close-mobile-nav");
+
+mobileNav.addEventListener(
+  "touchmove",
+  (e) => {
+    e.preventDefault();
+  },
+  false
+);
+
+mobileHam.addEventListener("click", () => {
+  mobileNav.classList.remove("mobile-nav-hidden");
+  html.classList.add("overscroll-disable");
+});
+
+mobileCancel.addEventListener("click", () => {
+  mobileNav.classList.add("mobile-nav-hidden");
+  html.classList.remove("overscroll-disable");
+});
+
+let listItems = mobileNav.childNodes[3].children;
+
+console.log(listItems[0].firstChild);
+console.log(listItems.length);
+
+for (let i = 0; i < listItems.length; i++) {
+  if (listItems[i].classList.contains("mobile-nav-hr")) continue;
+  listItems[i].firstChild.addEventListener("click", () => {
+    mobileNav.classList.add("mobile-nav-hidden");
+    html.classList.remove("overscroll-disable");
+  });
+}
