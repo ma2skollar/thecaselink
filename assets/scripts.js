7041c4618ae794d2e469d9b621dc3dfd906775ee
Matúš <kollarm9@gmail.com>
2023-06-03 02:07:31 +0200

    deleted images and cleaned up js

--- assets/scripts.js
+++ assets/scripts.js
@@ -3,6 +3,9 @@ const mobileHam = document.querySelector(".mobile-ham");
 const html = document.querySelector("html");
 const mobileCancel = document.querySelector(".close-mobile-nav");
 
+let listItems = mobileNav.childNodes[3].children;
+
+
 mobileHam.addEventListener("click", () => {
   mobileNav.classList.remove("mobile-nav-hidden");
   html.classList.add("overscroll-disable");
@@ -12,11 +15,6 @@ mobileCancel.addEventListener("click", () => {
   mobileNav.classList.add("mobile-nav-hidden");
   html.classList.remove("overscroll-disable");
 });
-
-let listItems = mobileNav.childNodes[3].children;
-
-console.log(listItems[0].firstChild);
-console.log(listItems.length);
 
 for (let i = 0; i < listItems.length; i++) {
   if (listItems[i].classList.contains("mobile-nav-hr")) continue;
