2e37fddd000467996d1b82fda8617861ce01e5d4
Matúš <kollarm9@gmail.com>
2023-07-19 08:41:27 +0200

    fixed bad body scroll ios

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,6 +1,7 @@
 const mobileNav = document.querySelector(".mobile-nav");
 const mobileHam = document.querySelector(".mobile-ham");
 const html = document.querySelector("html");
+const body = document.querySelector("body");
 const mobileCancel = document.querySelector(".close-mobile-nav");
 
 let listItems = mobileNav.childNodes[3].children;
@@ -9,11 +10,13 @@ let listItems = mobileNav.childNodes[3].children;
 mobileHam.addEventListener("click", () => {
   mobileNav.classList.remove("mobile-nav-hidden");
   html.classList.add("overscroll-disable");
+  body.classList.add("fix-body");
 });
 
 mobileCancel.addEventListener("click", () => {
   mobileNav.classList.add("mobile-nav-hidden");
   html.classList.remove("overscroll-disable");
+  body.classList.remove("fix-body");
 });
 
 for (let i = 0; i < listItems.length; i++) {
@@ -21,5 +24,6 @@ for (let i = 0; i < listItems.length; i++) {
   listItems[i].firstChild.addEventListener("click", () => {
     mobileNav.classList.add("mobile-nav-hidden");
     html.classList.remove("overscroll-disable");
+    body.classList.remove("fix-body");
   });
 }
