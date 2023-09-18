0106567eb03290bf1eae5eaa66afac1d7eb56b50
Matúš <kollarm9@gmail.com>
2023-09-19 00:44:35 +0200

    switched form added scroll up arrow

--- assets/scripts.js
+++ assets/scripts.js
@@ -3,6 +3,7 @@ const mobileHam = document.querySelector(".mobile-ham");
 const html = document.querySelector("html");
 const body = document.querySelector("body");
 const mobileCancel = document.querySelector(".close-mobile-nav");
+const scrollUpArrow = document.querySelector('.scroll-up-arrow-container');
 
 let listItems = mobileNav.childNodes[3].children;
 
@@ -27,3 +28,19 @@ for (let i = 0; i < listItems.length; i++) {
     body.classList.remove("fix-body");
   });
 }
+
+document.addEventListener('scroll', () => {
+  const bodyRect = body.getBoundingClientRect()
+  const scrollUpArrowRect = scrollUpArrow.getBoundingClientRect()
+  const offset = scrollUpArrowRect.top - bodyRect.top;
+  console.log(window.innerHeight * 1.1)
+  console.log(bodyRect.top)
+  console.log(scrollUpArrowRect.top)
+  if (offset > (window.innerHeight * 1.1)) scrollUpArrow.classList.add('visible-scroll-up-arrow')
+  else scrollUpArrow.classList.remove('visible-scroll-up-arrow')
+});
+
+scrollUpArrow.addEventListener('click', () => {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+});
